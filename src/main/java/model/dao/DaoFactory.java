624217923d994f957b7b74c1aa6a1a31d496b648
luiseduardo85 <77890900+luiseduardo85@users.noreply.github.com>
2025-03-15 05:43:08 -0300

    findById implementation

--- src/main/java/model/dao/DaoFactory.java
+++ src/main/java/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import Db.Db;
 import model.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao(){
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(Db.getConnection());
     }
 }
